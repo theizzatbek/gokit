c011ef190dda88c5c1f68a67191477fe75369af7
pulatov.izzatbek <pulatov.izzatbek@wb.ru>
2026-05-24 19:19:25 +0500

    style(auth): gofmt -w errors.go and errors_test.go

--- auth/errors_test.go
+++ auth/errors_test.go
@@ -4,21 +4,21 @@ import "testing"
 
 func TestErrorCodes_AreStableStrings(t *testing.T) {
 	cases := map[string]string{
-		"missing_token":          CodeMissingToken,
-		"invalid_token_scheme":   CodeInvalidTokenScheme,
-		"invalid_token":          CodeInvalidToken,
-		"expired_token":          CodeExpiredToken,
-		"missing_refresh":        CodeMissingRefresh,
-		"refresh_invalid":        CodeRefreshInvalid,
-		"refresh_expired":        CodeRefreshExpired,
-		"refresh_reused":         CodeRefreshReused,
-		"invalid_credentials":    CodeInvalidCredentials,
-		"missing_scope":          CodeMissingScope,
-		"missing_role":           CodeMissingRole,
-		"invalid_factory_args":   CodeInvalidFactoryArgs,
-		"password_hash_corrupt":  CodePasswordHashCorrupt,
-		"key_not_loaded":         CodeKeyNotLoaded,
-		"store_unavailable":      CodeStoreUnavailable,
+		"missing_token":         CodeMissingToken,
+		"invalid_token_scheme":  CodeInvalidTokenScheme,
+		"invalid_token":         CodeInvalidToken,
+		"expired_token":         CodeExpiredToken,
+		"missing_refresh":       CodeMissingRefresh,
+		"refresh_invalid":       CodeRefreshInvalid,
+		"refresh_expired":       CodeRefreshExpired,
+		"refresh_reused":        CodeRefreshReused,
+		"invalid_credentials":   CodeInvalidCredentials,
+		"missing_scope":         CodeMissingScope,
+		"missing_role":          CodeMissingRole,
+		"invalid_factory_args":  CodeInvalidFactoryArgs,
+		"password_hash_corrupt": CodePasswordHashCorrupt,
+		"key_not_loaded":        CodeKeyNotLoaded,
+		"store_unavailable":     CodeStoreUnavailable,
 	}
 	for want, got := range cases {
 		if got != want {
